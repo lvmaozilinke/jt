64c0732f0c0c43ce1c83dc48ec817f170da86e60
lvmaozilink <2468852146@qq.com>
2020-06-09 19:33:50 +0800

    智能交通

--- 智能交通/src/com/example/wangpengfei2/dialog/GetParkRateDialogextendsBaseDialog.java
+++ 智能交通/src/com/example/wangpengfei2/dialog/GetParkRateDialogextendsBaseDialog.java
@@ -1,6 +1,13 @@
 package com.example.wangpengfei2.dialog;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
 import com.example.wangpengfei2.R;
+import com.example.wangpengfei2.network.HttpAPI;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -9,13 +16,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 @SuppressLint("ValidFragment")
 public class GetParkRateDialogextendsBaseDialog extends Base {
 	
 	Context mcontext;
-	EditText mRateEt;
-	EditText mMoneyEt;
+	TextView mRateTv;
+	TextView mMoneyTv;
 
 	public GetParkRateDialogextendsBaseDialog(Context mcontext,
 			Context mcontext2) {
@@ -28,11 +37,45 @@ public class GetParkRateDialogextendsBaseDialog extends Base {
 
 		super.onActivityCreated(arg0);
 		InitView();
+		getData();
+	}
+
+	private void getData() {
+		HttpAPI.getInstance(mcontext).GetParkRate(new Listener<JSONObject>() {
+
+			@Override
+			public void onResponse(JSONObject arg0) {
+				try {
+					if(arg0.has("result")){
+						Toast.makeText(getActivity(),"≤È—Ø ß∞‹",Toast.LENGTH_LONG).show();
+						return;
+						}
+					mRateTv.setText(arg0.getString("RateType"));
+					mMoneyTv.setText(arg0.getString("Money"));
+					
+				} catch (JSONException e) {
+					e.printStackTrace();
+					
+				}
+				
+			}
+		}, new ErrorListener() {
+
+			@Override
+			public void onErrorResponse(VolleyError arg0) {
+				
+				
+				
+			}
+		});
+		
+
+		
 	}
 
 	private void InitView() {
-		mRateEt=(EditText)getView().findViewById(R.id.show_ratetype_tv);
-		mMoneyEt=(EditText)getView().findViewById(R.id.show_rateJe_tv);
+		mRateTv=(TextView)getView().findViewById(R.id.show_ratetype_tv);
+		mMoneyTv=(TextView)getView().findViewById(R.id.show_rateJe_tv);
 		
 	}
 
